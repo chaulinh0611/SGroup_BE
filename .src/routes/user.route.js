e436544c28e03ce35cfe223960f73f724b4a3475
Nguyễn Châu Linh <chaulinh0611z@gmail.com>
2025-04-23 17:38:16 +0700

    Update user.route.js

--- .src/routes/user.route.js
+++ .src/routes/user.route.js
@@ -1,11 +1,13 @@
-import { Router } from "express"
-import userController, * as UserController from "../controllers/user.controller.js"
-import ValidateMiddleware from "../middleware/validate.middleware.js"
+import { Router } from "express";
+import userController from "../controllers/user.controller.js";
+import ValidateMiddleware from "../middleware/validate.middleware.js";
 
-const route = Router()
-route.post('/', userController.createUser);
-route.get('/', userController.getAllUsers);
-route.get('/:id', ValidateMiddleware.validateId, userController.getUser); 
-route.put('/:id', ValidateMiddleware.validateId, userController.updateUser); 
-route.delete('/:id', ValidateMiddleware.validateId, userController.deleteUser);
-export default route
+const route = Router();
+
+route.post("/", ValidateMiddleware.validateName, ValidateMiddleware.validateEmail, userController.createUser);
+route.get("/", userController.getAllUsers);
+route.get("/:id", userController.getUser);
+route.put("/:id", ValidateMiddleware.validateName, userController.updateUser);
+route.delete("/:id", userController.deleteUser);
+
+export default route;
